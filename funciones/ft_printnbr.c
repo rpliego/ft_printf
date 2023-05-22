df5d805c97c52b2f60bb0565589a5233518eb2ba
Roberto <rpliego@cbr6s2.42barcelona.com>
2023-05-22 20:11:03 +0200

    en proceso por el printnbr

--- funciones/ft_printnbr.c
+++ funciones/ft_printnbr.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printstr.c                                      :+:      :+:    :+:   */
+/*   ft_printnbr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpliego <rpliego@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/21 12:22:06 by rpliego           #+#    #+#             */
-/*   Updated: 2023/05/21 12:22:07 by rpliego          ###   ########.fr       */
+/*   Created: 2023/05/22 20:05:57 by rpliego           #+#    #+#             */
+/*   Updated: 2023/05/22 20:05:58 by rpliego          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libftprintf.h>
+#include "libftprintf.h"
 
-int	ft_printstr(int out, char *str)
+int	ft_printnbr(int out, long int n)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
+	if (n < 0)
 	{
-		if (write(1, &str[i], 1) == -1)
-			return (-1);
-		out += 1;
-		i++;
+		n = n * (-1);
 	}
-	return (out);
+
 }
--- funciones/ft_printvoid.c
+++ funciones/ft_printvoid.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printstr.c                                      :+:      :+:    :+:   */
+/*   ft_printvoid.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpliego <rpliego@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/21 12:22:06 by rpliego           #+#    #+#             */
-/*   Updated: 2023/05/21 12:22:07 by rpliego          ###   ########.fr       */
+/*   Created: 2023/05/22 17:42:14 by rpliego           #+#    #+#             */
+/*   Updated: 2023/05/22 17:42:16 by rpliego          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libftprintf.h>
+#include "libftprintf.h"
 
-int	ft_printstr(int out, char *str)
+int	ft_printvoid(int out, void *ptr)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (write(1, &str[i], 1) == -1)
-			return (-1);
-		out += 1;
-		i++;
-	}
-	return (out);
+	
 }
